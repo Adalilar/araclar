f690c7ced832077c00f4cdbe21fe8703e6f74616
Adalilar <adalilarwebyazilim@gmail.com>
2020-12-25 08:32:10 +0300

    Update colab-google.sh

--- colab-google.sh
+++ colab-google.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m adali
-sudo adduser adali sudo
-echo 'adali:8426' | sudo chpasswd
+sudo useradd -m said
+sudo adduser said sudo
+echo 'said:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
