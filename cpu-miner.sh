2ca047d6a8a00edb92fce05097257b15caca9673
Adalilar <adalilarwebyazilim@gmail.com>
2021-03-07 22:41:43 +0300

    create cpu-miner.sh
    worker olmadan direk rplant indirip kazıyor

---
+++ cpu-miner.sh
@@ -0,0 +1,3 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qm96nztu0raw50y7mmv9yz0z0fx2u2xzd5xtva8
