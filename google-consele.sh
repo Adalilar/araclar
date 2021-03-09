4a5ab4654a9f4089fa7bc1dcd6bacf52a54379ac
Adalilar <adalilarwebyazilim@gmail.com>
2021-03-09 09:00:33 +0300

    gereksiz silindi

--- google-consele.sh
+++
@@ -1,7 +0,0 @@
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz
-while [ 1 ]; do
-./cpuminer-sse2 -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qm96nztu0raw50y7mmv9yz0z0fx2u2xzd5xtva8.muratyuce_gconsole
-sleep 5
-done
-
